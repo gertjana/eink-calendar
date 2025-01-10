e75a0b701db23371c760feb2e9d4bc5dda3c64f3
Gertjan Assies <gertjan.assies@gmail.com>
2025-01-10 20:13:01 +0100

    added python and run script and a default config, ignoring credentails/tokens and configs with secrets

--- google_cal.py
+++ google_cal.py
@@ -1,5 +1,5 @@
 import datetime
-import os.path
+import os
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -10,8 +10,11 @@ from googleapiclient.errors import HttpError
 from inky.auto import auto
 from inky.inky_uc8159 import CLEAN
 import inky
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 from font_source_sans_pro import SourceSansProSemibold
+import configparser
+import requests
+from io import BytesIO
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
@@ -23,21 +26,17 @@ large_font = ImageFont.truetype(SourceSansProSemibold, 48)
 def drawEvent(canvas, disp, event, offset):
   start = event["start"].get("dateTime", event["start"].get("date"))
   dt = datetime.datetime.fromisoformat(start).strftime('%a %-d %b %Y, %H:%M')
-  print(start, event["summary"])
   canvas.text((10,offset), dt + " " + event["summary"], disp.BLACK, normal_font)
   if "location" in event.keys():
     canvas.text((10, offset+24), event["location"], disp.BLACK,small_font)
 
-def getLatestEventsFromGoogleCalendar(maxEvents):
+def getLatestEventsFromGoogleCalendar(maxEvents, cal_id):
   creds = Credentials.from_authorized_user_file("token.json", SCOPES)
 
   try:
     service = build("calendar", "v3", credentials=creds)
 
-    # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-    cal_id = "f2580995446ab1540fa30e2e6193c19bb3dad048042dfef783d5e42fa8c7a853@group.calendar.google.com"
-    # print("Getting the upcoming 10 events")
     events_result = (
         service.events()
         .list(
@@ -54,45 +53,108 @@ def getLatestEventsFromGoogleCalendar(maxEvents):
     return []
   return events_result.get("items", [])
 
-def main():
-  print("Getting latest events")
-  events = getLatestEventsFromGoogleCalendar(6)
+def getWeather(api_key, lat, lon):
+  url = 'https://api.openweathermap.org/data/3.0/onecall?units=metric&lat=' + lat + '&lon=' + lon +'&appid=' + api_key + '&exclude=minutely,hourly,alerts'
+  info=requests.get(url).json()
+  daily=info["daily"][0]
+  weather = daily["weather"][0]
+  return {
+          "temp": daily["temp"]["day"], 
+          "feel": daily["feels_like"]["day"], 
+          "pressure": daily["pressure"], 
+          "humidity": daily["humidity"], 
+          "main": weather["main"],
+          "desc": weather["description"],
+          "icon": weather["icon"]
+         }
 
-  if not events:
-    print("No upcoming events found.")
-    return
+def getIcon(icon):
+  icon_url = "http://openweathermap.org/img/w/" + icon + ".png";
+  return requests.get(icon_url).content
 
-  print(f"got {len(events)} events")
+def getQuote():
+  quote = requests.get('https://zenquotes.io?api=random').json()[0]
+  return f'"{quote["q"]}" - {quote["a"]}'
 
-  print("init display")
+def split_line(line, split_at):
+    words = line.split()
+    lines = [' '.join(words[i:i + split_at]) for i in range(0, len(words), split_at)]
+    return '\n'.join(lines)
+
+
+
+
+def updateDisplay(config, weather, quote, events):
   disp = auto(ask_user=True, verbose=True)
-
-  print("Cleaning display")
   for _ in range(2):  
     for y in range(disp.height - 1):
       for x in range(disp.width - 1):
           disp.set_pixel(x, y, CLEAN)
-
-  print("Create canvas")
+  
+  TOPBAR_LOWER_Y = 100
+  BOTTOMBAR_UPPER_Y = 438
+  MARGIN = 10
+  WEATHER_NAMES_X = 450
+  WEATHER_VALUES_X = 560
+  ICON_LEFT = 700
+  EVENT_HEIGHT = 50
   img = Image.new("P", disp.resolution, disp.WHITE)
+
+  img.paste(disp.BLUE, (0, 0, disp.width-1, TOPBAR_LOWER_Y))
+  img.paste(disp.BLUE, (0, BOTTOMBAR_UPPER_Y, disp.width-1, disp.height-1))
+  icon_data = BytesIO(getIcon(weather["icon"]))
+  icon_img = Image.open(icon_data).convert("RGBA").resize((100,100))
+  img.paste(icon_img, (ICON_LEFT,MARGIN), mask=icon_img)
+
   canvas = ImageDraw.Draw(img)
+  
+  canvas.text((MARGIN, 0), datetime.date.today().strftime("%a %d %b"), disp.WHITE, large_font)
+  canvas.text((MARGIN, 68), f"Last updated: {datetime.datetime.today().strftime('%H:%M')}", disp.WHITE, small_font)
+  
+  canvas.text((ICON_LEFT, 2), f'{weather["main"]}', disp.WHITE, small_font)
 
-  canvas.text((10,10), datetime.date.today().strftime("%d %B %Y"), disp.BLACK, large_font)
-  canvas.line([(0,94),(disp.width-1,94)],disp.BLACK, 2)
-
-  y = 96
+  canvas.text((WEATHER_NAMES_X, 2),  'Temperature',        disp.WHITE, small_font)
+  canvas.text((WEATHER_NAMES_X, 18), 'Feels like ',        disp.WHITE, small_font)
+  canvas.text((WEATHER_NAMES_X, 34), 'Pressure',           disp.WHITE, small_font)
+  canvas.text((WEATHER_NAMES_X, 50), 'Humidity',           disp.WHITE, small_font)
+ 
+  canvas.text((WEATHER_VALUES_X, 2),  f'{round(weather["temp"],0)} °C',    disp.YELLOW, small_font)
+  canvas.text((WEATHER_VALUES_X, 18), f'{round(weather["feel"],0)} °C',    disp.YELLOW, small_font)
+  canvas.text((WEATHER_VALUES_X, 34), f'{weather["pressure"]} P',          disp.YELLOW, small_font)
+  canvas.text((WEATHER_VALUES_X, 50), f'{round(weather["humidity"],0)} %', disp.YELLOW, small_font)
+  canvas.text((WEATHER_VALUES_X, 68), f'{weather["desc"]}',                disp.YELLOW, small_font)
+  
+  y = TOPBAR_LOWER_Y
   for event in events:
     drawEvent(canvas, disp, event, y)
-    y += 64
+    y += EVENT_HEIGHT
 
-  last_updated = datetime.datetime.today().strftime('%a %-d %b %Y, %H:%M')
-  canvas.text((10, disp.height-20), f"Last updated: {last_updated}", disp.BLACK, small_font)
-  
+  canvas.multiline_text((MARGIN,BOTTOMBAR_UPPER_Y), quote, disp.WHITE, small_font)
+
   disp.set_image(img)
-
   disp.show()
-  
 
+def main():
+  config=configparser.ConfigParser()
+  try:
+    config.read_file(open(os.getcwd() + '/config.txt'))
+    lat = config.get('openweathermap', 'LAT', raw=False)
+    lon = config.get('openweathermap', 'LON', raw=False)
+    api_key = config.get('openweathermap', 'API_KEY', raw=False)
+    cal_id =  config.get('googlecalendar', 'CALENDAR_ID', raw=False)
+  except:
+    print('Error getting config values')
+  
+  weather = getWeather(api_key, lat, lon)
+
+  quote = split_line(getQuote(),20)
+
+  events = getLatestEventsFromGoogleCalendar(6, cal_id)
+  if not events:
+    print("No upcoming events found.")
+    return
+
+  updateDisplay(config, weather, quote, events)
 
 if __name__ == "__main__":
   main()
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /home/gertjan/.virtualenvs/pimoroni/bin/activate
+cd /home/gertjan/frame
+
+python google_cal.py
+
